2320a1f959a95d77073c3b37d8b585400692a64f
lanhuageng <lanhuageng@126.com>
2017-10-19 11:00:22 +0800

    Delete Person.js

--- js/test/Person.js
+++
@@ -1,8 +0,0 @@
-Class("com.Person", "com.Base", {
-  constructor : function(name) {
-    this.name = name || "zhangsan";
-  },
-  say : function(){
-    console.log(this.name + ", hello.");
-  }
-});
